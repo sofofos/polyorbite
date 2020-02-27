3621dd0f3f9acd6d2186f7da66e4cac0a69cf267
soalv <sofia.alvarez@polymtl.ca>
2020-02-27 15:08:47 -0500

    see previous msg

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,9 @@
 	skel.breakpoints({
 		xlarge:	'(max-width: 1680px)',
 		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
+		small:	'(max-width: 980px)',
+		xsmall:	'(max-width: 736px)',
+		xxsmall:	'(max-width: 480px)',
 	});
 
 	$(function() {
